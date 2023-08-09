5b9d51a87842bafb81c67791e12eb5e631c35d1a
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-08-09 15:42:30 -0400

    added second field test

--- java/src/test/com/google/code/samples/oauth2/OAuth2SaslClientFactoryTests.java
+++ java/src/test/com/google/code/samples/oauth2/OAuth2SaslClientFactoryTests.java
@@ -1,6 +1,9 @@
 package main.com.google.code.samples.oauth2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Field;
+
 import static org.junit.Assume.assumeTrue;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,7 @@ import main.com.google.code.samples.oauth2.OAuth2SaslClient;
 public class OAuth2SaslClientFactoryTests {
 
     private final OAuth2SaslClientFactory oAuth2SaslClientFactory = new OAuth2SaslClientFactory();
+    private final OAuth2SaslClient oAuth2SaslClient = new OAuth2SaslClient();
 
     @DisplayName("Assumptions needed for tests")
     @Test
@@ -40,5 +44,13 @@ public class OAuth2SaslClientFactoryTests {
     {
         OAuth2SaslClient oAuth2SaslClient = new OAuth2SaslClient();
         assertEquals(oAuth2SaslClient.getMechanismName(), oAuth2SaslClientFactory.getMechanismNames(null)[0]);
+    }
+
+    void getMechanismNameXOAUTHFromProperty() throws NoSuchFieldException, IllegalAccessException
+    {
+        Field f = OAuth2SaslClient.class.getDeclaredField("MechanismName");
+        f.setAccessible(true);
+        String name = (String)f.get(this.oAuth2SaslClient);
+        assertEquals(name, oAuth2SaslClientFactory.getMechanismNames(null)[0]);
     }
 }
